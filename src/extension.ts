e48a2feef8372348fb621103a6dfde7557a7a2e2
Alexandre <77246542+Lax3n@users.noreply.github.com>
2024-02-10 00:29:34 +0100

    - de comentaire

--- src/extension.ts
+++ src/extension.ts
@@ -11,32 +11,24 @@ export function activate(context: vscode.ExtensionContext) {
         const filePath = editor.document.fileName;
         const fileName = filePath.split('\\').pop();
 
-        // Check if the file is a Scilab script
+ 
         if (!fileName || !fileName.endsWith('.sce')) {
             vscode.window.showErrorMessage('The file is not a Scilab script.');
             return;
         }
 
-        // Assurez-vous d'avoir le bon répertoire de travail dans Scilab
         const workingDir = filePath.replace('\\' + fileName, '');
         const scilabCommand = `exec("./${fileName}", -1);`;
 
         // Launch Scilab console
-        // const scilabPath = 'C:\\Path\\To\\Scilab\\bin\\WScilex-cli.exe'; // Update to the actual path
-        // const terminal = vscode.window.createTerminal(`Scilab`);
         const existingTerminal = vscode.window.terminals.find(terminal => terminal.name === "Scilab");
         const terminal = existingTerminal || vscode.window.createTerminal(`Scilab`);
         if (!existingTerminal) {
-            terminal.sendText(`& "${scilabPath}" -nb -noatomsautoload -nouserstartup -f ./${fileName}`, true);
+            terminal.sendText(`& "${scilabPath}" -f ./${fileName}`, true);
         } else {
             // If terminal exists, simply run the script without initializing Scilab again
             terminal.sendText(`exec("./${fileName}", -1);`, true);
         }
-
-        // Wait for Scilab to initialize and then run the script
-        // setTimeout(() => {
-        //     terminal.sendText(scilabCommand, true);
-        // }, 5000); // Adjust delay as needed for Scilab to initialize
 
         terminal.show();
     });
